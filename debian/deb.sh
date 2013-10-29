c738b1a749bdc5be84ab92b4f3c299646665e751
Gregwar <g.passault@gmail.com>
2013-10-29 17:59:09 +0100

    Adding deb.sh

---
+++ debian/deb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REPOSITORY="$HOME/fatcat/"
+NAME=fatcat
+
+if [ $# -lt 1 ]; then
+ echo "Usage: ./deb.sh version"
+ exit
+fi
+
+WORKDIR=`pwd`
+VERSION=$1
+rm -rf $WORKDIR/$NAME*
+
+# Create the archive and the directory
+UPSTREAM=$WORKDIR/fatcat_$VERSION.orig.tgz
+cd $REPOSITORY &&
+git archive --format=tar.gz debian_$VERSION > $UPSTREAM &&
+cd $WORKDIR &&
+mkdir $NAME &&
+cd $NAME &&
+tar zxvf $UPSTREAM &&
+
+# Run the debian packaging
+dpkg-buildpackage -us -uc -ai386 &&
+
+cd $WORKDIR &&
+scp fatcat_$VERSION* root@gregwar.com:/home/www/gregwar2/fatcat/

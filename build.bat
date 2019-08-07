a106f866fdbf3b14944689f1392af7d033fbf04f
Inbar Barkai <inbarbarkai@gmail.com>
2019-08-07 16:19:44 +0300

    Added script to build fatcat with windows

---
+++ build.bat
@@ -0,0 +1,5 @@
+if not exist build md build
+cd build
+git clean -fdx && cmake .. && "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" fatcat.sln  /p:OutDir=bin
+cd ..
+pause
